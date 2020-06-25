2380e0d4effb5ae37e4504cb4eafff8199596235
enas <enas.salah.b@gmail.com>
2020-06-26 00:08:16 +0200

    Updates

---
+++ precache-manifest.6c0f609b2eb0e2c3618f0453c41b88d0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83ba167522587f43d58a66cbe374b29",
+    "url": "/ReactJSProject/index.html"
+  },
+  {
+    "revision": "1b83ecb296795ac437df",
+    "url": "/ReactJSProject/static/css/2.9d7b8a20.chunk.css"
+  },
+  {
+    "revision": "44045bdfeb3acaedfd96",
+    "url": "/ReactJSProject/static/css/main.7813d069.chunk.css"
+  },
+  {
+    "revision": "1b83ecb296795ac437df",
+    "url": "/ReactJSProject/static/js/2.89707e7e.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/ReactJSProject/static/js/2.89707e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44045bdfeb3acaedfd96",
+    "url": "/ReactJSProject/static/js/main.70c49fae.chunk.js"
+  },
+  {
+    "revision": "98641bb6d658784c9dcc",
+    "url": "/ReactJSProject/static/js/runtime-main.ea13b4a4.js"
+  },
+  {
+    "revision": "8f65a03390d620df8a81faebb72bffb4",
+    "url": "/ReactJSProject/static/media/React.svg.8f65a033.png"
+  },
+  {
+    "revision": "6817cc3b53b1ec073bf927fc5acff54a",
+    "url": "/ReactJSProject/static/media/Vue.svg.6817cc3b.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactJSProject/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactJSProject/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactJSProject/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactJSProject/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactJSProject/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ca37af3315e93571d845ad13a588301f",
+    "url": "/ReactJSProject/static/media/iti.ca37af33.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJSProject/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b6b8512d400b5b4ba4ec796e14bed18d",
+    "url": "/ReactJSProject/static/media/mypic.b6b8512d.jpg"
+  }
+]);
